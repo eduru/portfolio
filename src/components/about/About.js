37291c5eac677e09d74d4ac0dbe0ad31e3a9d75d
Eduardo Ruiz <eduru@me.com>
2021-09-28 11:25:25 +0900

    working on about component

---
+++ src/components/about/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./About.css";
+import eduardoRuiz from "../../media/eduardoRuiz";
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-desc">
+        <h3> Let me tell you something about me:</h3>
+        <p>
+          mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare
+          lectus sit amet est placerat in egestas erat imperdiet sed euismod
+          nisi porta lorem mollis aliquam ut porttitor leo
+        </p>
+      </div>
+      <div className="about-img">
+        <img src={eduardoRuiz} alt="Eduardo Ruiz" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
