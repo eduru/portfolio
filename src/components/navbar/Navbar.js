10a61ce9d40668bace83d97ac1723f4be33566af
Eduardo Ruiz <eduru@me.com>
2021-09-28 11:05:44 +0900

    working in navbar  component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = ({ isScrolling }) => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Eduardo Ruiz</div>
+    </nav>
+  );
+};
+
+export default Navbar;
