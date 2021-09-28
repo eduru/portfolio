efa72b9861e5d8adf67f36d13489e9463d7ebae5
Eduardo Ruiz <eduru@me.com>
2021-09-28 11:10:13 +0900

    added styles to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = ({ isScrolling }) => {
+  const backToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">Eduardo Ruiz</div>
+    <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
+      <div className="navbar-logo" onClick={backToTop}>
+        Eduardo Ruiz
+      </div>
     </nav>
   );
 };
